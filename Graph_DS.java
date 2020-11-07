003fcab35f89c809776ad5750b4e7a7788eb02be
MarkMichaely <44968872+MarkMichaely@users.noreply.github.com>
2020-11-07 16:48:38 +0200

    0.01
    First try, doesn't work well.
    Before trying to use HashMap in Graph_DS

---
+++ Graph_DS.java
@@ -0,0 +1,177 @@
+package ex0;
+
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * this class represents an undirectional unweighted graph.
+ * It should support a large number of nodes (over 10^6, with average degree of 10).
+ */
+public class Graph_DS implements graph {
+    private Set<node_data> _nS; //node set
+    private int _vertex;
+    private int _edges;
+    private int _modeCount;
+
+    public Graph_DS() {
+        this._nS = new HashSet<node_data>();
+        this._vertex = 0;
+        this._edges = 0;
+        this._modeCount = 0;
+    }
+
+    @Override
+    /**
+     * return the node_data by the node_id,
+     * @param key - the node_id
+     * @return the node_data by the node_id, null if none.
+     */
+    public node_data getNode(int key) {
+        if (this._nS != null) {
+            Iterator<node_data> it = _nS.iterator();
+            while (it.hasNext()) {
+                node_data node = it.next();
+                if (node.getKey() == key) return node;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    /**
+     * * return true iff (if and only if) there is an edge between node1 and node2
+     * 	 * Note: this method should run in O(1) time.
+     */
+    public boolean hasEdge(int node1, int node2) {
+        if (this.getNode(node1)
+                .hasNi(node2)
+                && this.getNode(node2).
+                hasNi(node1))
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    /**
+     * 	 * add a new node to the graph with the given node_data.
+     * 	 * Note: this method should run in O(1) time.
+     */
+    public void addNode(node_data n) {
+        if (this.getNode(n.getKey()) == null) {
+            this._nS.add(n);
+            this._vertex++;
+            this._modeCount++;
+        }
+    }
+
+    @Override
+    /**
+     * Connect an edge between node1 and node2.
+     *   Note: this method should run in O(1) time.
+     *   Note2: if the edge node1-node2 already exists - the method simply does nothing.
+     */
+    public void connect(int node1, int node2) {
+        if (!this.hasEdge(node1, node2)) {
+            this.getNode(node1).
+                    addNi(getNode(node2));
+            this._edges++;
+            this._modeCount++;
+        }
+    }
+
+    @Override
+    /**
+     * This method return a pointer (shallow copy) for the
+     * collection representing all the nodes in the graph.
+     * Note: this method should run in O(1) time.
+     */
+    public Collection<node_data> getV() {
+        return this._nS;
+    }
+
+    @Override
+    /**
+     * This method returns a collection containing all the
+     * nodes connected to node_id
+     * Note: this method should run in O(1) time.
+     */
+    public Collection<node_data> getV(int node_id) {
+        return this.getNode(node_id).
+                getNi();
+    }
+
+    @Override
+    /**
+     * Delete the node (with the given ID) from the graph -
+     * and removes all edges which starts or ends at this node.
+     * This method should run in O(n), |V|=n, as all the edges should be removed.
+     * @return the data of the removed node (null if none).
+     */
+    public node_data removeNode(int key) {
+        node_data node=this.getNode(key);
+        if (node != null) {
+            Iterator<node_data> it = this.getV(key)
+                    .iterator();
+            HashSet<node_data> set=new HashSet<node_data>();
+                while (it.hasNext()) {
+                    node_data n=it.next();
+                    set.add(n);
+                    it.remove();
+                }
+                Iterator<node_data> itr=set.iterator();
+                while (itr.hasNext()){
+                    node_data n=itr.next();
+                    removeEdge(n.getKey(),key);
+                }
+            this._vertex--;
+            this._modeCount++;
+            this.getV().remove(node);
+        }
+        return node;
+    }
+
+    @Override
+    /**
+     * Delete the edge from the graph,
+     * Note: this method should run in O(1) time.
+     */
+    public void removeEdge(int node1, int node2) {
+        this.getNode(node1).removeNode(this.getNode(node2));
+        this._edges--;
+        this._modeCount++;
+
+    }
+
+    @Override
+    /** return the number of vertices (nodes) in the graph.
+     * Note: this method should run in O(1) time.
+     * @return number of vertex
+     */
+    public int nodeSize() {
+        return _vertex;
+    }
+
+    @Override
+    /**
+     * return the number of edges (undirectional graph).
+     * Note: this method should run in O(1) time.
+     * @return number of edges
+     */
+    public int edgeSize() {
+        return _edges;
+    }
+
+    @Override
+    /**
+     * return the Mode Count - for testing changes in the graph.
+     * Any change in the inner state of the graph should cause an increment in the ModeCount
+     * @return number of changes
+     */
+    public int getMC() {
+        return _modeCount;
+    }
+}
---
+++ NodeData.java
@@ -0,0 +1,121 @@
+package ex0;
+
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
+/**
+ * this class represents a node (vertex) in an (undirectional) unweighted graph.
+ * further reading :
+ * https://en.wikipedia.org/wiki/Vertex_(geometry)
+ */
+public class NodeData implements node_data {
+    private static int _num_of_nodes = 0;
+    private int _key;
+    private Set<node_data> _neighbors;
+    private String _info;
+    private int _tag;
+
+
+    public NodeData() {
+        this._key = _num_of_nodes;
+        this._neighbors = new HashSet<node_data>();
+        this._info = null;
+        this._tag = 0;
+        _num_of_nodes++;
+
+    }
+
+    @Override
+    /**
+     function returns node's key, each node has a diffrent key
+     */
+    public int getKey() {
+        return this._key;
+    }
+
+    /**
+     * function returns a collection with all this node's neighbor nodes
+     */
+    @Override
+    public Collection<node_data> getNi() {
+        return this._neighbors;
+    }
+
+    @Override
+    /**
+     * function return true if a node is adjacent to this node
+     *
+     */
+    public boolean hasNi(int key) {
+        Iterator<node_data> it = _neighbors.iterator();
+        while (it.hasNext()) {
+            node_data node = it.next();
+            if (node.getKey() == key) return true;
+        }
+        return false;
+    }
+
+    @Override
+    /**
+     * This method adds the node_data (t) to this node.
+     * creates an edge between the two nodes.
+     */
+    public void addNi(node_data t) {
+        if (t.getKey() != this.getKey()) {
+            this.getNi().add(t);
+            t.getNi().add(this);
+        }
+    }
+
+    @Override
+    /**
+     * Removes the edge this-node,
+     */
+    public void removeNode(node_data node) {
+        this.getNi().remove(node);
+        node.getNi().remove(this);
+    }
+
+    @Override
+    /**
+     * return the remark (meta data) associated with this node.
+     */
+    public String getInfo() {
+        return this._info;
+    }
+
+    @Override
+    /**
+     *  Allows changing the remark (meta data) associated with this node.
+     */
+    public void setInfo(String s) {
+        this._info = s;
+    }
+
+    @Override
+    /**
+     * 	 * Temporal data (aka color: e,g, white, gray, black)
+     * 	 * which can be used be algorithms
+     * 	 * @return
+     *          */
+
+    public int getTag() {
+        return this._tag;
+    }
+
+    @Override
+    /**
+     *  * Allow setting the "tag" value for temporal marking an node - common
+     * 	 * practice for marking by algorithms.
+     * 	 * @param t - the new value of the tag
+     */
+    public void setTag(int t) {
+        this._tag = t;
+    }
+
+}
+
